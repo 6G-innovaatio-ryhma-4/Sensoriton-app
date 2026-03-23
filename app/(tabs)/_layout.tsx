368d7be1c4db7c2833f9eb2c424a066e26abbe0b
Hajax8 <t2luha01@students.oamk.fi>
2026-03-23 18:44:38 +0200

    simulaatiota aloitettu

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,31 +3,28 @@ import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { main } from '@/assets/colors';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Koti',
+          tabBarIcon: ({ color}) => <IconSymbol size={28} name="house.fill" color={main.accent} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="simulaatio"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Vaihda Tilanne',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={main.accent} />,
         }}
       />
     </Tabs>
